228aed77eb6520b86729a912dcd6b2b10d335710
Blake Rosenthal <brosenthalpdx@gmail.com>
2015-04-10 08:58:33 -0700

    data writeup

--- Well_map.R
+++ Well_map.R
@@ -4,9 +4,17 @@ well_logs <- filter(well_logs, !is.na(latitude))
 well_logs <- filter(well_logs, !is.na(longitude))
 well_logs <- filter(well_logs, !is.na(depth_first_water))
 well_logs <- filter(well_logs, !is.na(max_yield))
+well_logs <- filter(well_logs, !is.na(start_date))
 
 well_logs <- filter(well_logs, longitude >= -124.5, longitude <= -116.5)
 well_logs <- filter(well_logs, latitude >= 42, latitude <= 46.5)
+
+well_logs <- mutate(well_logs, numeric_date = start_date)
+well_logs$numeric_date <- as.numeric(gsub("/", "", well_logs$start_date))
+well_logs$numeric_date <- (well_logs$numeric_date %% 100)
+
+well_logs <- filter(well_logs, numeric_date < 16)
+well_logs <- filter(well_logs, numeric_date > 10)
 
 
 library(ggmap)
@@ -26,5 +34,5 @@ ggmap(Map,
       xlab = "Longitude",
       legend = "right") +
   geom_point(data = well_logs, 
-             color = "red",
-             aes(x = longitude, y = latitude))
+             #color = "red",
+             aes(x = longitude, y = latitude, color = well_logs$depth_first_water))
